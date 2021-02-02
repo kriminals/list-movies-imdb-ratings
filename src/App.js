60352496824f625e45403eac321deb822d66e8d4
George Tzinetas <krimini.kriminals@gmail.com>
2021-02-02 17:49:44 +0200

    Removed  demo with bugs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import Movies from './Movies';
+import React, { useState, useEffect } from 'react';
+import SearchComponent from './SearchComponent';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+     <SearchComponent /> 
+    </div>
+  );
+}
+
+export default App;
