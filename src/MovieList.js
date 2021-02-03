9615ba19f3ea766de481d83830674504ae39fd87
George Tzinetas <krimini.kriminals@gmail.com>
2021-02-03 23:03:01 +0200

    Add cards, star ratings, movie posters and plot

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,40 +1,56 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
+import { Card, Rate, Row, Col } from 'antd';
 
 function MovieList(props) {
-    const API_KEY = process.env.REACT_APP_API_KEY;
-    const [movieData, setMovieData] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    useEffect(() => {
-      let didCancel = false;
-      props.imdbId.forEach((id) => {
-        const fetchImdbRating = async () => {
-          const result = await axios(
-            `https://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`
-          );
-          if (!didCancel) {setMovieData(movieData => [...movieData, result.data]);}
-          
-          setIsLoading(true);
-          
-        };
-        fetchImdbRating();
-      });
-      return () => {didCancel = true}
-    }, [props.imdbId]);
-    const printMovies = movieData.map(movie => 
-      <li key={movie.imdbID.toString()}>{movie.Title} Rating: {movie.imdbRating}</li>
-     )
-  
-    return (
-      <div>
-        <Fragment>
-        <ul>
-          {printMovies}
-        </ul>
-        </Fragment>
-      </div>
-    )
-  
-  }
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const [movieData, setMovieData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  export default MovieList;
+  useEffect(() => {
+    let didCancel = false;
+    props.imdbId.forEach((id) => {
+      const fetchImdbRating = async () => {
+        const result = await axios(
+          `https://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`
+        );
+        if (!didCancel) {
+          setMovieData((movieData) => [...movieData, result.data]);
+        }
+        console.log(movieData);
+        setIsLoading(true);
+      };
+      fetchImdbRating();
+    });
+    return () => {
+      didCancel = true;
+    };
+  }, [props.imdbId]);
+  const printMovies = movieData.map((movie) => (
+    <Col span={6}>
+      <Card
+        title={movie.Title}
+        cover={<img alt="Movie Poster" src={movie.Poster} />}
+      >
+        Rating: {movie.imdbRating}{' '}
+        <Rate
+          count={10}
+          disabled
+          allowHalf={true}
+          defaultValue={movie.imdbRating}
+        />
+        <p>{movie.Plot}</p>
+      </Card>
+    </Col>
+  ));
+
+  return (
+    <div>
+      <Fragment>
+        <Row justify="space-around">{printMovies}</Row>
+      </Fragment>
+    </div>
+  );
+}
+
+export default MovieList;
